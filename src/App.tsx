f46a7c0387b77507e96140d2e6b560d04ce6e670
Theo <theo.belland.pro@gmail.com>
2026-06-03 13:14:02 +0200

    Finalisation de la sidebar.     -> Mise en place de la structure de base de la sidebar.     -> Ajout de la fonctionnalité d'ouverture et de fermeture de la sidebar.     -> Ajout de la fonctionnalité de recherche dans la sidebar.     -> Ajout de la fonctionnalité de sous-menu dans la sidebar.

--- src/App.tsx
+++ src/App.tsx
@@ -1,38 +1,70 @@
-import './App.css'
+import {useState} from 'react';
+import {BsArrowLeftShort, BsSearch, BsChevronDown, BsFillImageFill, BsReverseLayoutTextSidebarReverse, BsPerson} from 'react-icons/bs';
+import {AiFillEnvironment, AiOutlineBarChart, AiOutlineFileText, AiOutlineMail, AiOutlineSetting, AiOutlineLogout} from 'react-icons/ai';
+import {RiDashboardFill} from 'react-icons/ri';
 
-function App() {
+const App = () => {
+  const [isOpen, setIsOpen] = useState(false);
+  const [submenuOpen, setSubmenuOpen] = useState(false);
+  const Menus = [
+    { title: "Dashboard" },
+    { title: "Pages", icon: <AiOutlineFileText /> },
+    { title: "Media", spacing: true, icon: <BsFillImageFill/> },
+    {
+      title: "Projects",
+      icon : <BsReverseLayoutTextSidebarReverse/>,
+      submenu: true,
+      submenuItems: [{ title: "Submenu 1" }, { title: "Submenu 2" }, { title: "Submenu 3" }],
+    },
+    { title: "Analytics", icon: <AiOutlineBarChart/> },
+    {title: "Inbox", icon: <AiOutlineMail/>},
+    { title: "Profile", spacing: true, icon: <BsPerson/> },
+    { title: "Setting", icon: <AiOutlineSetting/> },
+    { title: "Logout",  icon: <AiOutlineLogout/> },
+  ];
 
   return (
-      <div className="bg-white">
-        <div className="mx-auto max-w-7xl py-24 sm:px-6 sm:py-32 lg:px-8">
-          <div className="relative isolate overflow-hidden bg-gray-900 px-6 pt-16 shadow-2xl sm:rounded-3xl sm:px-16 md:pt-24 lg:flex lg:gap-x-20 lg:px-24 lg:pt-0">
-            <svg viewBox="0 0 1024 1024" aria-hidden="true" className="absolute top-1/2 left-1/2 -z-10 size-256 -translate-y-1/2 mask-[radial-gradient(closest-side,white,transparent)] sm:left-full sm:-ml-80 lg:left-1/2 lg:ml-0 lg:-translate-x-1/2 lg:translate-y-0">
-              <circle r="512" cx="512" cy="512" fill="url(#759c1415-0410-454c-8f7c-9a820de03641)" fill-opacity="0.7" />
-              <defs>
-                <radialGradient id="759c1415-0410-454c-8f7c-9a820de03641">
-                  <stop stop-color="#7775D6" />
-                  <stop offset="1" stop-color="#E935C1" />
-                </radialGradient>
-              </defs>
-            </svg>
-            <div className="mx-auto max-w-md text-center lg:mx-0 lg:flex-auto lg:py-32 lg:text-left">
-              <h2 className="text-3xl font-semibold tracking-tight text-balance text-white sm:text-4xl">Boost your productivity. Start using our app today.</h2>
-              <p className="mt-6 text-lg/8 text-pretty text-gray-300">Ac euismod vel sit maecenas id pellentesque eu sed consectetur. Malesuada adipiscing sagittis vel nulla.</p>
-              <div className="mt-10 flex items-center justify-center gap-x-6 lg:justify-start">
-                <a href="#" className="rounded-md bg-white px-3.5 py-2.5 text-sm font-semibold text-gray-900 shadow-xs hover:bg-gray-100 focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-white"> Get started </a>
-                <a href="#" className="text-sm/6 font-semibold text-white hover:text-gray-100">
-                  Learn more
-                  <span aria-hidden="true">→</span>
-                </a>
-              </div>
-            </div>
-            <div className="relative mt-16 h-80 lg:mt-8">
-              <img width="1824" height="1080" src="https://tailwindcss.com/plus-assets/img/component-images/dark-project-app-screenshot.png" alt="App screenshot" className="absolute top-0 left-0 w-228 max-w-none rounded-md bg-white/5 ring-1 ring-white/10" />
-            </div>
-          </div>
+    <div className="flex">
+      <div className={`bg-dark-purple h-screen p-5 pt-8 ${isOpen ? 'w-72' : 'w-20'} duration-300 relative`}>
+        <BsArrowLeftShort className={`bg-white text-dark-purple text-3xl rounded-full absolute -right-3 top-9 border border-dark-purple cursor-pointer ${!isOpen && 'rotate-180'}`} onClick={() => setIsOpen(!isOpen)} />
+        <div className="inline-flex">
+          <AiFillEnvironment className={`bg-amber-300 text-4xl rounded cursor-pointer block float-left mr-2 duration-500 ${isOpen && 'rotate-[360deg]'}`} />
+          <h1 className={`text-white origin-left font-medium text-xl duration-300 ${!isOpen && 'scale-0'}`}>Sidebar</h1>
         </div>
+        <div className={`flex items-center rounded-md bg-light-white mt-6 ${!isOpen ? "px-2.5" : "px-4"} py-2`}>
+          <BsSearch className={`text-white text-lg cursor-pointer mr-2 ${!isOpen && "mr-2"}`} />
+          <input type={"search"} placeholder="Search" className={`text-base bg-transparent w-full text-white focus:outline-none ${!isOpen && "hidden"}`} />
+        </div>
+        <ul className="pt-2">
+          {Menus.map((Menu, index) => (
+            <li key={index} className={`text-gray-300 text-sm flex flex-col gap-y-2 cursor-pointer p-2 hover:bg-light-white rounded-md ${Menu.spacing ? 'mt-9' : 'mt-2'}`}>
+              <div className="flex items-center gap-x-4">
+                <span className="text-2xl block float-left">
+                  {Menu.icon ? Menu.icon : <RiDashboardFill />}
+                </span>
+                <span className={`text-base font-medium flex-1 duration-200 ${!isOpen && "hidden"}`}>{Menu.title}</span>
+                {Menu.submenu && isOpen && (
+                  <BsChevronDown className={`${submenuOpen ? 'rotate-180' : ''}`} onClick={() => setSubmenuOpen(!submenuOpen)} />
+                )}
+              </div>
+              {Menu.submenu && submenuOpen && isOpen && (
+                <ul className="w-full">
+                  {Menu.submenuItems.map((submenuItem, subIndex) => (
+                    <li key={subIndex} className="text-gray-300 text-sm flex items-center gap-x-4 cursor-pointer p-2 px-5 hover:bg-light-white rounded-md ml-12">
+                      {submenuItem.title}
+                    </li>
+                  ))}
+                </ul>
+              )}
+            </li>
+          ))}
+        </ul>
       </div>
-  )
-}
+    </div>
 
-export default App
+
+
+  );
+};
+
+export default App;
